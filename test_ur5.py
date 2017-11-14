33381d1b887fcedf21688473476c7014c021e71e
Rafael Papallas <sc13rp@leeds.ac.uk>
2017-11-14 14:05:40 +0000

    Minor change to make the loading of URDF and SRDF dynamic

--- test_ur5.py
+++ test_ur5.py
@@ -1,12 +1,18 @@
 from openravepy import *
 import numpy
+import os
 
 env = Environment()
 env.SetViewer('qtcoin')
 module = RaveCreateModule(env, 'urdf')
 
 with env:
-    name = module.SendCommand('LoadURI /home/rafael/Documents/ur5_openrave/ur5.urdf /home/rafael/Documents/ur5_openrave/ur5.srdf')
+    # Get the path of SRDF and URDF from the current working directory.
+    current_working_directory = os.getcwd()
+    srdf_path = current_working_directory + "/ur5_model/ur5.srdf"
+    urdf_path = current_working_directory + "/ur5_model/ur5.urdf"
+
+    name = module.SendCommand('LoadURI {} {}'.format(urdf_path, srdf_path))
     robot = env.GetRobot(name)
 
 # Add the robot to the environment
